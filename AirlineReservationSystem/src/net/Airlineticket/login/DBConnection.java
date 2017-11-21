13faeedef6b8ab490e531ce754a8082cd4640713
TheFortune <31195068+TheFortune@users.noreply.github.com>
2017-11-21 00:14:12 -0500

    Jar file is uploaded to the lib folder and classpath Connection is established and working also. I was able to insert data into the database by typing queries in java

---
+++ AirlineReservationSystem/src/net/Airlineticket/login/DBConnection.java
@@ -0,0 +1,42 @@
+package net.Airlineticket.login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DBConnection {
+	
+public static void main(String[] args) {
+		
+		try {
+			// 1. Get a connection to the database
+			String userName = "root";
+			   String password = "lilson12";
+			   String url = "jdbc:mysql://localhost:3306/demojava?useSSL=false";
+			Connection Conn = DriverManager.getConnection(url, userName, password);	
+			
+			// 2. Create a statement
+			Statement myStatement = Conn.createStatement();
+			// 3. execute the sql query
+			//ResultSet Rs = myStatement.executeQuery("SELECT * FROM user");
+			
+			String sql = "INSERT INTO user " 
+						+ "(username, email, password)"
+						+ "VALUES ('jhon', 'jon@mail.com', 'test123')";
+			myStatement.executeUpdate(sql);
+			
+			System.out.println("Insert complete");
+						
+			// 4. Process result
+		//	while(Rs.next()) {
+		//		System.out.println(Rs.getString("username") + " , " + Rs.getString("email"));
+		//	}
+			
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
